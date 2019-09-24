67cdb7ec6a34a96b5f0a11ae0b01d9ea1878f547
Lucas058 <lucasbrandao.a@gmail.com>
2019-09-24 11:03:44 -0300

    Aula 146 - Iniciando calsses Tabuleiro e pecas

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,14 +1,12 @@
 package aplicacao;
 
-import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Posicao p = new Posicao(3, 5);
-		
-		System.out.println(p);
+		Tabuleiro t = new Tabuleiro(3, 5);
 		
 	}
 
